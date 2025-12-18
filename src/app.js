1dae657b1a8cff7dab5729fa2ef1223d430bbfb3
priyanshu kumar <priyanshu.official.1408@gmail.com>
2025-12-18 15:19:06 +0530

    mongoose setup

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import connectToSocket from "./controllers/socketManager.js";
+
+import cors from "cors";
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port" , (process.env.PORT || 8000))
+
+app.set("pors", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://priyanshuofficial1408_db_user:Priya1408@cluster0.iv5ygns.mongodb.net/")
+    console.log(`MONGO connected DB Host: ${connectionDb.connection.host}`)
+    app.listen(app.get("port"), ()=>{
+        console.log("listen on port 8000")
+    });
+}
+
+start();
