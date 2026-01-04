0a5186b9f43482a4e4504679180d2982f9450f7d
priyanshu kumar <priyanshu.official.1408@gmail.com>
2026-01-04 11:07:49 +0530

    auth page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import {Route , BrowserRouter as Router , Routes} from 'react-router-dom';
 import LandingPage from './pages/landing';
 import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
 
 function App() {
   return (
     <>
     <Router>
+      <AuthProvider>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/auth' element = {<Authentication/>}/>
+        <Route path='/:url' element={<VideoMeetComponent/>}/>
       </Routes>
+      </AuthProvider>
     </Router>
     </>
   );
---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const server_url = "http://localhost:8000";
+
+export default function VideoMeetComponent() {
+  return (
+    <div>
+      VideoMeetComponent
+    </div>
+  )
+}
